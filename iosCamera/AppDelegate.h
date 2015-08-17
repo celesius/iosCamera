a7ce0068c92b6fcb72e04b7472d23a0f7aee4d67
BoJiang <188166614@qq.com>
2015-08-17 22:04:50 +0800

    Initial Commit

---
+++ iosCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iosCamera
+//
+//  Created by vk on 15/8/17.
+//  Copyright (c) 2015年 quxiu8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iosCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iosCamera
+//
+//  Created by vk on 15/8/17.
+//  Copyright (c) 2015年 quxiu8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
