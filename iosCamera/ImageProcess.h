1d3bc8532486bcb5720cb365a559d9cd459a57b4
BoJiang <188166614@qq.com>
2016-02-02 12:03:42 +0800

    更新文件描述

--- iosCamera/ImageProcess.h
+++ iosCamera/ImageProcess.h
@@ -3,7 +3,7 @@
 //  iosCamera
 //
 //  Created by vk on 15/10/9.
-//  Copyright © 2015年 quxiu8. All rights reserved.
+//  Copyright © 2015年 Clover. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
